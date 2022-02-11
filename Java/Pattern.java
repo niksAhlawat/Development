fe43fc858c9af6031a43af352379761db7a6e6b6
niksAhlawat <nikhilahlawat.nikk@outlook.com>
2022-02-11 23:26:27 +0530

    Floyd's Triangle

--- Java/Pattern.java
+++ Java/Pattern.java
@@ -323,9 +323,29 @@ public class Pattern {
 	}	    
     }
 
+    /**
+     Floyd's Triangle Pattern
+      1
+      2  3
+      4  5  6
+      7  8  9  10
+      11 12 13 14 15
+     */
+    private void pattern18(int n){
+        int a = 1;
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String arga[]){
         Pattern p = new Pattern();
-        p.pattern17(5);
+        p.pattern11(5);
     }
 }
 
